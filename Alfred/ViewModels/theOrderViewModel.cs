ec5dae91db4c517399a9761e4526c88c0ac5daf3
Joey Hendrich <jlh1100@yahoo.com>
2019-06-12 15:44:31 -0500

    stage 1

--- Alfred/ViewModels/theOrderViewModel.cs
+++ Alfred/ViewModels/theOrderViewModel.cs
@@ -61,5 +61,57 @@ namespace Alfred.ViewModels
                 return countList;
             }
         }
+        public static List<int> BasketAverages()
+        {
+            using (TheOrderModelDataContext db = new TheOrderModelDataContext("Data Source=10.1.10.114,1434;Initial Catalog=benDover;Persist Security Info=True;User ID=ugoadmin;Password=ugo-2019"))
+            {
+                var q = (from x in db.theORDERs select x);
+                q.ToList();
+
+                int one = 0, two = 0, three = 0,
+                    four = 0, five = 0, overFive = 0;
+
+                foreach (var z in q)
+                {
+                    // if total is less than or equal 9.99
+                    if (z.BasketSize.CompareTo(Convert.ToDecimal(1.00)) <= 0)
+                    {
+                        one++;
+                    }
+                    else if (z.BasketSize.CompareTo(Convert.ToDecimal(2.00)) <= 0)
+                    {
+                        two++;
+                    }
+                    else if (z.BasketSize.CompareTo(Convert.ToDecimal(3.00)) <= 0)
+                    {
+                        three++;
+                    }
+                    else if (z.BasketSize.CompareTo(Convert.ToDecimal(4.00)) <= 0)
+                    {
+                        four++;
+                    }
+                    else if (z.BasketSize.CompareTo(Convert.ToDecimal(5.00)) <= 0)
+                    {
+                        five++;
+                    }
+                    else if (z.BasketSize.CompareTo(Convert.ToDecimal(5.00)) > 0)
+                    {
+                        overFive++;
+                    }
+                }
+
+                List<int> countList = new List<int>
+                {
+                    one,
+                    two,
+                    three,
+                    four,
+                    five,
+                    overFive
+                };
+
+                return countList;
+            }
+        }
     }
 }
--- Alfred/Views/ChartA.xaml.cs
+++ Alfred/Views/ChartA.xaml.cs
@@ -15,30 +15,41 @@ namespace Alfred.Views
         public ChartA()
         {
             InitializeComponent();
-
             List<int> countList = new List<int>();
             countList = TheOrderViewModel.OrderRanges();
 
-
-            SeriesCollection = new SeriesCollection
-            {
-                new ColumnSeries
-                {
-                    Title = "Number of Orders",
-                    Values = new ChartValues<int> { countList[0], countList[1], countList[2], countList[3],
-                    countList[4]}
-                }
-            };
-
-            SeriesCollection[0].Values.Add(countList[5]);
-
-            Labels = new[] { "0 - 10", "10 - 15", "15 - 20", "20 - 25", "25 - 30", "over 30" };
+            Values = new ChartValues<int> { countList[0], countList[1], countList[2], countList[3],
+                     countList[4]};
+            Labels = new[] { "$0 - $10", "$10 - $15", "$15 - $20", "$20 - $25", "$25 - $30", "over $30" };
             Formatter = value => value.ToString("N");
-
             DataContext = this;
+
         }
 
-        public SeriesCollection SeriesCollection { get; set; }
+        //    List<int> countList = new List<int>();
+        //    countList = TheOrderViewModel.OrderRanges();
+
+
+            //    SeriesCollection = new SeriesCollection
+            //    {
+            //        new ColumnSeries
+            //        {
+            //            Title = "Number of Orders",
+            //            Values = new ChartValues<int> { countList[0], countList[1], countList[2], countList[3],
+            //            countList[4]}
+            //        }
+            //    };
+
+            //    SeriesCollection[0].Values.Add(countList[5]);
+
+            //    Labels = new[] { "$0 - $10", "$10 - $15", "$15 - $20", "$20 - $25", "$25 - $30", "over $30" };
+            //    Formatter = value => value.ToString("N");
+
+            //    DataContext = this;
+            //}
+
+            //public SeriesCollection SeriesCollection { get; set; }
+        public ChartValues<int> Values { get; set; }
         public string[] Labels { get; set; }
         public Func<double, string> Formatter { get; set; }
     }
